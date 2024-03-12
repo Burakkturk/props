6b6292dbcb2ff1cd1c82ee929efb2c848625505e
Burakkturk <burak1845turk@icloud.com>
2024-03-12 03:54:56 +0300

    Destr.

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,15 +1,17 @@
 import Name from "./Name"
 
-const Person = (props) => {
-    console.log(props)
+const Person = ({name, img, color}) => {
+   
+
+    // const {name,img,color} = props
   return (
     <div>
         
-        <Name name={props.name}/>
+        <Name name={name}/>
         {/* <p>Hello {props.name}</p> */}
-        <img src={props.img}/>
+        <img src={img}/>
         <br></br>
-       <p> color= {props.color} </p>
+       <p> color= {color} </p>
 
         {/* <img src="https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_1280.jpg"
         alt="cat"/> */}
